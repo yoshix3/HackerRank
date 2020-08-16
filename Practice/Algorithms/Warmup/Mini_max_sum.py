5cacfb9b9599c5884c63c2ded7948216a8cc6f8d
yoshi-PC\yoshi <y.nara.2000@gmail.com>
2020-08-16 11:00:18 +0900

    Mini-Max Sum

---
+++ Practice/Algorithms/Warmup/Mini_max_sum.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the miniMaxSum function below.
+def miniMaxSum(arr):
+    print("{} {}".format(sum(arr)-max(arr), sum(arr)-min(arr)))
+
+
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
